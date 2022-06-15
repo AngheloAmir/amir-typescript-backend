b218c6b681e54f1a3d40e1b9f8f06d266ad22a2f
AngheloAmir <71964853+AngheloAmir@users.noreply.github.com>
2022-06-15 19:04:14 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,8 @@
 import '../env/startInitEnv';
 import app from './Server';
 import Logger from 'jet-logger';
-const logger = new Logger();
-
 
 const port = Number(process.env.PORT || 3000);
 app.listen(port, () => {
-    logger.info('Express server started on port: ' + port);
+    Logger.info('Express server started on port: ' + port);
 });
